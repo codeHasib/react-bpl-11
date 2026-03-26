2cfed35cd9cda80382b6596208f94237069217b5
codeHasib <mohammadhasib9827@gmail.com>
2026-03-26 23:54:32 +0600

    card add

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -12,32 +12,32 @@ const Card = ({
 }) => {
   return (
     <div className="p-4 border rounded-2xl" >
-      <div>
+      <div className="space-y-3">
         <img className="h-65" src={img} alt="" />
-        <div>
         <h2 className="text-[18px] font-semibold" >{name}</h2>
-          <p> {country} </p>
+        <div className="flex justify-between items-center border-b pb-2" >
+          <p className="font-bold" > {country} </p>
           <button className="btn" > {position} </button>
         </div>
-        <div>
-            <h2>
+        <div className="flex justify-between items-center">
+            <h2 className="font-bold" >
                 Rating
             </h2>
-            <h2>
+            <h2 className="font-bold" >
                 {rating}
             </h2>
         </div>
-        <div>
-            <h2>
+        <div className="flex justify-between items-center" >
+            <h2 className="font-light" >
                 {battingStyle}
             </h2>
-            <h2>
+            <h2 className="font-light" >
                 {bowlingStyle}
             </h2>
         </div>
-        <div>
+        <div className="flex justify-between items-center">
             <h2>
-                Price: {price}
+                Price: ${price}
             </h2>
             <button className="btn" >
                 Choose Player
